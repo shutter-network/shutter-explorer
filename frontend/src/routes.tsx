4cabb0afa4836211871ecfe57e83fc299fa38573
ylembachar <your.email@example.com>
2024-08-28 13:50:00 +0200

    add not found page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Transaction from "./pages/Transaction";
 import Slot from "./pages/Slot";
 import System from "./pages/System";
+import NotFound from "./components/NotFound";
 
 const AppRoutes = () => {
     return (
@@ -10,7 +11,8 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/system-overview" element={<System />} />
                 <Route path="/slot-overview" element={<Slot />} />
-                <Route path="/transaction-detail" element={<Transaction />} />
+                <Route path="/transaction" element={<Transaction />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
