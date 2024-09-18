65b996c693b4ce2d72357d0a793963455feededf
faheelsattar <faheelsattar@yahoo.com>
2024-09-18 14:45:13 +0500

    export styles

--- frontend/src/components/BasicTable.tsx
+++ frontend/src/components/BasicTable.tsx
@@ -1,5 +1,5 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, tableCellClasses } from "@mui/material";
-import styled from "styled-components";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { StyledTableCell, StyledTableRow } from "../styles/table";
 
 interface Column {
     id: string;
@@ -11,26 +11,6 @@ interface BasicTableProps<T> {
     rows: T[];
     columns: Column[];
 }
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-    },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-        border: 0,
-    },
-}));
 
 export default function BasicTable<T extends { [key: string]: any }>({
     rows,
