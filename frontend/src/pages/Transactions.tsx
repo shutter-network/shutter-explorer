76ed5ff8369cf5d41144062900b9d3a681e67cfc
faheelsattar <faheelsattar@yahoo.com>
2024-09-19 20:46:33 +0500

    add transactions page

---
+++ frontend/src/pages/Transactions.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+import ResponsiveLayout from "../components/ResponsiveLayout"
+import TitleSection from "../components/TitleSection"
+
+const Transactions: FC = () => {
+    return (
+        <ResponsiveLayout>
+            <></>
+        </ResponsiveLayout>
+    )
+}
+
+export default Transactions;
