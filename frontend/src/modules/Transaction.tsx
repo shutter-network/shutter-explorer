5a84eb5bbcc55fcb1b96c255938eddffb368b010
ylembachar <your.email@example.com>
2024-09-27 12:10:01 +0200

    fix issue displaying the percentage values and update tooltips

--- frontend/src/modules/Transaction.tsx
+++ frontend/src/modules/Transaction.tsx
@@ -9,13 +9,14 @@ import { WebsocketEvent } from '../types/WebsocketEvent';
 
 const Transaction = () => {
     const { data: executedTransactionsData, loading: loadingExecutedTransactions, error: errorExecutedTransactions } = useFetch('/api/transaction/total_executed_transactions');
-    const { data: shutterizedTransactionsPerMonthData, loading: loadingShutterizedTransactionsPerMonth, error: errorShutterizedTransactionsPerMonth } = useFetch('/api/transaction/total_transactions_per_month');
-    const { data: shutterizedTransactionPercentageData, loading: loadingShutterizedTransactionPercentage, error: errorShutterizedTransactionPercentage } = useFetch('/api/transaction/transaction_percentage');
+    const { data: totalTransactionsPerMonthData, loading: loadingTotalTransactionsPerMonth, error: errorTotalTransactionsPerMonth } = useFetch('/api/transaction/total_transactions_per_month');
+    const { data: transactionPercentageData, loading: loadingTransactionPercentage, error: errorTransactionPercentage } = useFetch('/api/transaction/transaction_percentage');
 
-    const [executedTransactions, setExecutedTransactions] = useState<string | number>(executedTransactionsData?.message || 'N/A');
-    const [shutterizedTransactionsPerMonth, setShutterizedTransactionsPerMonth] = useState<string | number>(shutterizedTransactionsPerMonthData?.message || 'N/A');
-    const [shutterizedTransactionPercentage, setShutterizedTransactionPercentage] = useState<string | number>(shutterizedTransactionPercentageData?.percentage || 'N/A');
+    const [executedTransactions, setExecutedTransactions] = useState<number | null>(null);
+    const [transactionsPerMonth, setTransactionsPerMonth] = useState<number | null>(null);
+    const [transactionPercentage, setTransactionPercentage] = useState<number | null>(null);
     const [, setWebSocketError] = useState<string | null>(null);
+
 
     const { socket } = useWebSocket()!;
 
@@ -31,14 +32,14 @@ const Transaction = () => {
                         case 'total_executed_transactions_updated':
                             setExecutedTransactions(websocketEvent.Data);
                             break;
-                        case 'shutterized_transactions_per_month_updated':
+                        case 'transactions_per_month_updated':
                             if ('count' in websocketEvent.Data) {
-                                setShutterizedTransactionsPerMonth(websocketEvent.Data.count);
+                                setTransactionsPerMonth(websocketEvent.Data.count);
                             }
                             break;
-                        case 'shutterized_transaction_percentage_updated':
-                            if ('percentage' in websocketEvent.Data) {
-                                setShutterizedTransactionPercentage(websocketEvent.Data.percentage);
+                        case 'transaction_percentage_updated':
+                            if ('message' in websocketEvent.Data) {
+                                setTransactionPercentage(websocketEvent.Data.message);
                             }
                             break;
                         default:
@@ -59,47 +60,53 @@ const Transaction = () => {
                 socket.removeEventListener('error', handleError);
             };
         }
-
-
     }, [socket]);
 
     useEffect(() => {
-        if (executedTransactionsData?.message) setExecutedTransactions(executedTransactionsData.message);
-        if (shutterizedTransactionsPerMonthData?.message) setShutterizedTransactionsPerMonth(shutterizedTransactionsPerMonthData.message[0].TotalTxs);
-        if (shutterizedTransactionPercentageData?.percentage) setShutterizedTransactionPercentage(shutterizedTransactionPercentageData.percentage);
-    }, [executedTransactionsData, shutterizedTransactionsPerMonthData, shutterizedTransactionPercentageData]);
+        if (executedTransactionsData && executedTransactionsData.message !== undefined) {
+            setExecutedTransactions(Number(executedTransactionsData.message));
+        }
+
+        if (totalTransactionsPerMonthData && totalTransactionsPerMonthData.message) {
+            setTransactionsPerMonth(Number(totalTransactionsPerMonthData.message[0].TotalTxs));
+        }
+
+        if (transactionPercentageData && transactionPercentageData.message !== undefined) {
+            setTransactionPercentage(Number(transactionPercentageData.message)); // Fix here
+        }
+    }, [executedTransactionsData, totalTransactionsPerMonthData, transactionPercentageData]);
 
     return (
         <Box sx={{ flexGrow: 1, marginTop: 4 }}>
-                    <OverviewCard title="Transaction Overview" iconSrc={overviewIcon}>
-                        {errorExecutedTransactions ? (
-                            <Alert severity="error">Error fetching Executed Transactions: {errorExecutedTransactions.message}</Alert>
-                        ) : (
-                            <InfoBox
-                                title="# Successfully Executed Transactions"
-                                tooltip="Total number of successfully executed transactions"
-                                value={loadingExecutedTransactions ? 'Loading...' : executedTransactions}
-                            />
-                        )}
-                        {errorShutterizedTransactionsPerMonth ? (
-                            <Alert severity="error">Error fetching Shutterized Transactions per Month: {errorShutterizedTransactionsPerMonth.message}</Alert>
-                        ) : (
-                            <InfoBox
-                                title="# Shutterized Transactions per Month"
-                                tooltip="Number of Shutterized transactions executed in the last month"
-                                value={loadingShutterizedTransactionsPerMonth ? 'Loading...' : shutterizedTransactionsPerMonth}
-                            />
-                        )}
-                        {errorShutterizedTransactionPercentage ? (
-                            <Alert severity="error">Error fetching Shutterized Transaction Percentage: {errorShutterizedTransactionPercentage.message}</Alert>
-                        ) : (
-                            <InfoBox
-                                title="Percentage of Shutterized Transactions"
-                                tooltip="Percentage of transactions that are Shutterized"
-                                value={loadingShutterizedTransactionPercentage ? 'Loading...' : `${shutterizedTransactionPercentage}%`}
-                            />
-                        )}
-                    </OverviewCard>
+            <OverviewCard title="Transaction Overview" iconSrc={overviewIcon}>
+                {errorExecutedTransactions ? (
+                    <Alert severity="error">Error fetching Executed Transactions: {errorExecutedTransactions.message}</Alert>
+                ) : (
+                    <InfoBox
+                        title="# Successfully Executed Transactions"
+                        tooltip="Total number of successfully executed transactions"
+                        value={loadingExecutedTransactions || executedTransactions === null ? 'Loading...' : executedTransactions}
+                    />
+                )}
+                {errorTotalTransactionsPerMonth ? (
+                    <Alert severity="error">Error fetching Transactions per Month: {errorTotalTransactionsPerMonth.message}</Alert>
+                ) : (
+                    <InfoBox
+                        title="# Shutterized Transactions per Month"
+                        tooltip="Number of shielded transactions in the last 30 days"
+                        value={loadingTotalTransactionsPerMonth || transactionsPerMonth === null ? 'Loading...' : transactionsPerMonth}
+                    />
+                )}
+                {errorTransactionPercentage ? (
+                    <Alert severity="error">Error fetching Transaction Percentage: {errorTransactionPercentage.message}</Alert>
+                ) : (
+                    <InfoBox
+                        title="Percentage of Shielded Transactions"
+                        tooltip="Percentage of monthly shielded transactions amongst all gnosis transactions"
+                        value={loadingTransactionPercentage || transactionPercentage === null ? 'Loading...' : `${(transactionPercentage * 100).toFixed(2)}%`}
+                    />
+                )}
+            </OverviewCard>
         </Box>
     );
 };
